18cd782a52bfa483403062207b5db067845919fb
YutaIwashina <hal.pw50368@gmail.com>
2017-04-06 14:42:26 +0900

    20170406 autolayout、ボタン装飾など完了

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 各IBOutlet使用準備
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
-    
+    @IBOutlet weak var startAndStop: UIButton!
+    @IBOutlet weak var appTitle: UILabel!
     // タイマー準備
     var timer: Timer!
-    
-    // 画像番号
+    // 画像番号の設定
     var imageNo = 0
+    // 配列imageNameの要素数
+    var maxImageNo = 5
     
     // 画像呼び出し関数
     func dispImage() {
@@ -32,9 +35,42 @@ class ViewController: UIViewController {
         imageView.image = image
     }
     
+    // rgb変換関数
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 各ボタン、タイトルの背景色設定
+        backButton.backgroundColor = UIColorFromRGB(rgbValue: 0x90ee90)
+        nextButton.backgroundColor = UIColorFromRGB(rgbValue: 0x90ee90)
+        startAndStop.backgroundColor = UIColorFromRGB(rgbValue: 0x90ee90)
+        appTitle.backgroundColor = UIColorFromRGB(rgbValue: 0x87cefa)
+        
+        // imageView（表示する画像）、各ボタンのbackground、タイトルを角丸に
+        // imageView
+        self.imageView.layer.cornerRadius = 30
+        self.imageView.layer.masksToBounds = true
+        // 戻るボタン
+        self.backButton.layer.cornerRadius = 10
+        self.backButton.layer.masksToBounds = true
+        // 進むボタン
+        self.nextButton.layer.cornerRadius = 10
+        self.nextButton.layer.masksToBounds = true
+        // 再生/停止ボタン
+        self.startAndStop.layer.cornerRadius = 10
+        self.startAndStop.layer.masksToBounds = true
+        // タイトル
+        self.appTitle.layer.cornerRadius = 5
+        self.appTitle.layer.masksToBounds = true
         
         // 画像ファイル読み込み、表示
         let image = UIImage(named: "worldHeritage01")
@@ -64,7 +100,7 @@ class ViewController: UIViewController {
         // 画像切り替えのために画像番号に1を足す
         imageNo += 1
         // 最後に割り当てた画像が表示されたら、画像番号0の画像に戻る
-        if imageNo > 4 {
+        if imageNo > (maxImageNo - 1) {
             imageNo = 0
         }
         // 画像表示
@@ -75,14 +111,14 @@ class ViewController: UIViewController {
     @IBAction func backButton(_ sender: UIButton) {
         imageNo -= 1
         if imageNo < 0 {
-            imageNo = 4
+            imageNo = (maxImageNo - 1)
         }
         dispImage()
     }
     // 進むボタン処理
     @IBAction func nextButton(_ sender: UIButton) {
         imageNo += 1
-        if imageNo > 4 {
+        if imageNo > (maxImageNo - 1) {
             imageNo = 0
         }
         dispImage()
